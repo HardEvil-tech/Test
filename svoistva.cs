e6f1632ba161f466d62a8f47bc231a7cc98a8f4b
HardEvil <53075792+HardEvil-tech@users.noreply.github.com>
2022-09-17 12:50:59 +0300

    Add files via upload

---
+++ svoistva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp6
+{
+    public class Car
+    {
+        public string Company { get; set; }
+        public string Model  {  get; set; }
+        public double Speed { get; set; }
+}
+}
